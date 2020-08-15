1c527cd219b8f921920e2932005f32aef7e6d6d9
ogasawara.takehiko <ogasawara.t@sunmarie.com>
2020-08-15 14:40:21 +0900

    環境構築

--- mybotapp/config/puma.rb
+++ mybotapp/config/puma.rb
@@ -9,7 +9,10 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+#port        ENV.fetch("PORT") { 3000 }
+# socketの設定
+# ディレクトリがない場合は作成しておく
+bind "unix:///mybotapp/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
